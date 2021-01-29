4dcff8207b508b9cfeff7b3583c74cf7b636aca4
Lisovolenko Stas <lisostas@gmail.com>
2021-01-30 00:11:09 +0200

    Some new tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "register link and title" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", signup_path
   end
 end
