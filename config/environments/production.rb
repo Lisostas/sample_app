ed65494e34c7a1424de9bd4647dd5adad4b117c7
Lisovolenko Stas <lisostas@gmail.com>
2021-02-18 10:58:59 +0200

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
     :address              =>    'smtp.sendgrid.net',
     :port                 =>    '587',
     :authentication       =>    :plain,
-    :user_name            =>    "apikey",
-    :password             =>    <your-api-key>,
+    :user_name            =>    ENV['SENDGRID_USERNAME'],
+    :password             =>    ENV['SENDGRID_PASSWORD'],
     :domain               =>    'heroku.com',
     :enable_starttls_auto =>    true
   }
