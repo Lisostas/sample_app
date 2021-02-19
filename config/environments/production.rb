6a0f391a1a4ad2f6691e222cf7fcd394d05ba9a3
Lisovolenko Stas <lisostas@gmail.com>
2021-02-19 13:26:59 +0200

    trying sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     :port                 =>    '587',
     :authentication       =>    :plain,
     :user_name            =>    'apikey',
-    :password             =>    'APIKEYFORMYAPP',
+    :password             =>    ENV['APIKEYFORMYAPP'],
     :domain               =>    'heroku.com',
     :enable_starttls_auto =>    true
   }
